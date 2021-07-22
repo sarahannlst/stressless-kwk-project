cdc809482002ec206a88a2e8f3f6ec87cbe5eb43
fatima-79 <fatima.carmona3478@gmail.com>
2021-07-22 14:47:19 -0500

    lol

---
+++ stressless-kwk-project/WritePostViewController.swift
@@ -0,0 +1,47 @@
+//
+//  WritePostViewController.swift
+//  stressless-kwk-project
+//
+//  Created by Fatima Carmona on 7/22/21.
+//
+
+import UIKit
+
+class WritePostViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var typeHere: UITextField!
+    var postText = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func postButton(_ sender: Any) {
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showBlog"{
+          let destinationController = segue.destination as! SeePostViewController
+          destinationController.finalPost = typeHere.text!
+        }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
